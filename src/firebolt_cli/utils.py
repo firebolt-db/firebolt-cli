a562cd1c190795fdfd28c15e25b4beb16f1402ac
Yury Dzerin <yury@firebolt.io>
2022-01-19 13:56:34 +0100

    Implement list databases

--- src/firebolt_cli/utils.py
+++ src/firebolt_cli/utils.py
@@ -1,13 +1,40 @@
 import json
+from typing import Sequence
 
 from tabulate import tabulate
 
 
-def prepare_execution_result(data: dict, use_json: bool = False) -> str:
+def prepare_execution_result_line(
+    data: Sequence, header: Sequence, use_json: bool = False
+) -> str:
+    """
+    return the string representation of data in either json or tabular formats.
+    In case of json, the result is dict
+    In case of tabular, the result is table with headers in the first column
+    """
+
+    if len(data) != len(header):
+        raise ValueError("data and header have different length")
+
+    if use_json:
+        return json.dumps(dict(zip(header, data)), indent=4)
+    else:
+        return tabulate(list(zip(header, data)), tablefmt="grid")
+
+
+def prepare_execution_result_table(
+    data: Sequence[Sequence], header: Sequence, use_json: bool = False
+) -> str:
     """
     return the string representation of data in either json or tabular formats
+    In case of json, the result is list of dicts
+    In case of tabular, the result is table with headers in the first row
     """
+    for d in data:
+        if len(d) != len(header):
+            raise ValueError("data and header have different length")
+
     if use_json:
-        return json.dumps(data, indent=4)
+        return json.dumps([dict(zip(header, d)) for d in data], indent=4)
     else:
-        return tabulate(data.items(), tablefmt="grid")
+        return tabulate(data, headers=header, tablefmt="grid")
